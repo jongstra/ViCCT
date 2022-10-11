4e6e41e3363d6684c0ebde157eeba39efbfb065a
Thomas Jongstra <jongstra@gmail.com>
2022-10-11 16:51:54 +0200

    Dont open browser when starting voila for demo deployment.

--- start_vicct.sh
+++ start_vicct.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source venv/bin/activate && voila notebooks/make_image_prediction_voila.ipynb &
+source venv/bin/activate && voila notebooks/make_image_prediction_voila.ipynb --no-browser &
 lt --subdomain crowdcountingamsterdam --port 8866

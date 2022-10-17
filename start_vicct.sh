506b90674dd58d8de89e3d303ac7d5d4a8c28203
Thomas Jongstra <jongstra@gmail.com>
2022-10-17 14:47:33 +0200

    Changes starting script (still WIP).

--- start_vicct.sh
+++ start_vicct.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-source venv/bin/activate && voila notebooks/make_image_prediction_voila.ipynb --no-browser &
-lt --subdomain crowdcountingamsterdam --port 8866
+#source venv/bin/activate && voila notebooks/make_image_prediction_voila.ipynb --no-browser &
+#lt --subdomain amsterdamcrowdcounter --port 8866
+
+source venv/bin/activate
+# Add code to start notebook from terminal here.
+onboardbase tunnels:create -p 8800 -s amsterdamcrowdcounter

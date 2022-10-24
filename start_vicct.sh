6406e080803980ee78effac3ec623c1a740ebbfa
Thomas Jongstra <jongstra@gmail.com>
2022-10-24 14:51:43 +0200

    Small update.

--- start_vicct.sh
+++ start_vicct.sh
@@ -5,5 +5,5 @@
 
 source venv/bin/activate
 # Add code to start notebook from terminal here.
-while true; do lt --subdomain amsterdamcrowdcounter --port 8800 && break; done
-#while true; do onboardbase tunnels:create -p 8800 -s amsterdamcrowdcounter && break; done
+#while true; do lt --subdomain amsterdamcrowdcounter --port 7860 && break; done
+while true; do onboardbase tunnels:create -p 7860 -s amsterdamcrowdcounter && break; done
